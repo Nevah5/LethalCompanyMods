f64c3dbba0834b31ca22154a77ea9cdcd6bb652a
Nevah5 <neevaah5@gmail.com>
2024-01-04 17:11:43 +0100

    WIP

--- Plugin.cs
+++ Plugin.cs
@@ -1,14 +1,36 @@
-﻿using BepInEx;
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
 using BepInEx.Unity.Mono;
+using HarmonyLib;
 
 namespace all_my_fellas_radio;
 
 [BepInPlugin("dev.nevah5.mods.lethal-company.all-my-fellas-radio", "AllMyFellasRadio", "1.0.0")]
 public class Plugin : BaseUnityPlugin
 {
+    private static Plugin Instance;
+    
+    private const string Guid = "dev.nevah5.mods.lethal-company.all-my-fellas-radio";
+
+    private const string Name = "AllMyFellasRadio";
+
+    private const string Version = "1.0.0";
+    
     private void Awake()
     {
-        // Plugin startup logic
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        Instance = this;
+        var harmony = new Harmony(Guid);
+        harmony.PatchAll();
+        Logger.LogInfo($"Plugin {Name} is loaded!");
     }
+
+    #region logging
+    internal static void LogDebug(string message) => Instance.Log(message, LogLevel.Debug);
+    internal static void LogInfo(string message) => Instance.Log(message, LogLevel.Info);
+    internal static void LogWarning(string message) => Instance.Log(message, LogLevel.Warning);
+    internal static void LogError(string message) => Instance.Log(message, LogLevel.Error);
+    internal static void LogError(Exception ex) => Instance.Log($"{ex.Message}\n{ex.StackTrace}", LogLevel.Error);
+    private void Log(string message, LogLevel logLevel) => Logger.Log(logLevel, message);
+    #endregion
 }
